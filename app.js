d6c7c53c4321259166fabb3e243eec9ec2f97745
Melvin Lu <melvinlu1996@gmail.com>
2017-01-01 00:49:47 -0500

    Refactor code, add season rate, add current streak, prevent invalid summonery name query, disable summoner 5 until API key upgraded

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // TODO:
+// Minify files
 // Implement caching
 // Implement rate limit avoiding mechanism
 // Implement error handling mechanism
+// Allow 'enter' for submission (add event listener)
 // Hide API key
 
 // Declare modules to use
@@ -25,40 +27,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Socket.io functions
 io.on('connection', function (socket) {
-    // Function to calculate the win rates of each summoner
-    socket.on('Calculate Rates', function (summoners) {
-        var url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' +
+    // Function to calculate the ranked stats of each summoner
+    socket.on('Calculate Stats', function (summoners) {
+        url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' +
                 summoners.join() +
                 '?api_key=' +
                 API_KEY,
             summonerIdsMap = new Map(),
             summonerIdsArr = [];
         // Poll for the ID of each summoner
-        request(url)
+        request({url, resolveWithFullResponse: true})
             .then(function(response) {
-                var summonerData = JSON.parse(response);
+                var summonerData = JSON.parse(response.body);
                 var keys = Object.keys(summonerData);;
                 keys.forEach(function(key, index) {
                     summonerIdsMap.set(key, summonerData[key].id);
                     summonerIdsArr.push(summonerData[key].id);
                 });
-                calculateWinRates(summonerIdsMap, summonerIdsArr,
+                calculateRecentStats(summonerIdsMap, summonerIdsArr,
                     summoners, socket);
             })
             .catch(function(err) {
-                console.log(err);
+                socket.emit('Calculated Stats', [], [], []);
+                return;
             });
     });
 });
 
-function calculateWinRates(summonerIdsMap, summonerIdsArr, summoners, socket) {
+function calculateRecentStats(summonerIdsMap, summonerIdsArr, summoners, socket) {
     var summonerWRs = [],
         summonerRates = new Map(),
+        summonerStrks = [],
+        summonerStreaks = new Map(),
         baseUrl = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/',
-        url,
-        recentData,
-        gameCount,
-        wins,
         aggregateWins = 0,
         aggregateGames = 0;
 
@@ -67,16 +68,38 @@ function calculateWinRates(summonerIdsMap, summonerIdsArr, summoners, socket) {
         url = baseUrl + summonerId + '/recent?api_key=' + API_KEY;
         return request(url)
             .then(function(response) {
-                wins = 0;
-                recentData = JSON.parse(response);
-                gameCount = recentData.games.length;
-                for (var i = 0; i < gameCount; ++i, ++aggregateGames) {
-                    if (recentData.games[i].stats.win) {
-                        ++wins;
-                        ++aggregateWins;
+                var wins = 0;
+                var currentStreak = 0;
+                var rankedGames = 0;
+                var streakSet = false;
+                var recentData = JSON.parse(response);
+                var gameCount = recentData.games.length;
+                for (var i = 0; i < gameCount; ++i) {
+                    if (recentData.games[i].subType == 'RANKED_SOLO_5x5') {
+                        ++rankedGames;
+                        ++aggregateGames;
+                        if (!currentStreak) {
+                            currentStreak = (recentData.games[i].stats.win) ? 1 : -1;
+                        }
+                        if (recentData.games[i].stats.win) {
+                            ++wins;
+                            ++aggregateWins;
+                            if (!streakSet && currentStreak > 0) {
+                                if (i) ++currentStreak;
+                            } else {
+                                streakSet = true;
+                            }
+                        } else {
+                            if (!streakSet && currentStreak < 0) {
+                                if (i) --currentStreak;
+                            } else {
+                                streakSet = true;
+                            }
+                        }
                     }
                 }
-                summonerRates.set(summonerId, wins / gameCount * 100);
+                summonerRates.set(summonerId, Math.round(wins / rankedGames * 100));
+                summonerStreaks.set(summonerId, currentStreak);
             })
             .catch(function(err) {
                 console.log(err);
@@ -92,10 +115,73 @@ function calculateWinRates(summonerIdsMap, summonerIdsArr, summoners, socket) {
                         summonerIdsMap.get(
                             summoners[i]))
                         );
+                summonerStrks.push(
+                    summonerStreaks.get(
+                        summonerIdsMap.get(
+                            summoners[i]))
+                        );
             }
             summonerWRs.push(Math.round(aggregateWins / aggregateGames * 100));
-            // Send the data back to the client
-            socket.emit('Calculated Rates', summonerWRs);
+            // Now calculate all win rates for each summoner
+            calculateSeasonRates(summonerIdsMap, summonerIdsArr, summoners, summonerWRs,
+                                summonerStrks, socket);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+function calculateSeasonRates(summonerIdsMap, summonerIdsArr, summoners,
+    summonerWRs, summonerStrks, socket) {
+    var summonerSeasonWRs= [],
+        summonerSeasonRates = new Map(),
+        baseUrl = 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/',
+        aggregateWins = 0,
+        aggregateGames = 0;
+
+    // Queue the promise of each request
+    var promises = summonerIdsArr.map(function(summonerId) {
+        url = baseUrl + summonerId + '/summary?api_key=' + API_KEY;
+        return request(url)
+            .then(function(response) {
+                var summonerStats = JSON.parse(response);
+                var rankedIndex;
+                summonerStats.playerStatSummaries.forEach(function(summary, index) {
+                    if (summary.playerStatSummaryType == 'RankedSolo5x5') {
+                        rankedIndex = index;
+                    }
+                });
+                var wins = summonerStats.playerStatSummaries[rankedIndex].wins;
+                aggregateWins += wins;
+                var games = wins + summonerStats.playerStatSummaries[rankedIndex].losses;
+                aggregateGames += games;
+                if (games == 0) {
+                    summonerSeasonRates.set(summonerId, 0);
+                } else {
+                    summonerSeasonRates.set(summonerId, Math.round(wins / games * 100));
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        });
+
+    // Properly reorder and return the season win rate of each summoner
+    return Promise.all(promises)
+        .then(function(data) {
+            for (var i = 0; i < summoners.length; ++i) {
+                summonerSeasonWRs.push(
+                    summonerSeasonRates.get(
+                        summonerIdsMap.get(
+                            summoners[i]))
+                        );
+            }
+            if (!aggregateGames) {
+                summonerSeasonWRs.push(0);
+            } else {
+                summonerSeasonWRs.push(Math.round(aggregateWins / aggregateGames * 100));
+            }
+            socket.emit('Calculated Stats', summonerWRs, summonerSeasonWRs, summonerStrks);
         })
         .catch(function(err) {
             console.log(err);
